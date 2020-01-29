7820cdf0eee3b96d5717c70cd29f368cdb6997c4
Fridericus <32454643+Fridericus@users.noreply.github.com>
2020-01-29 20:43:36 +0000

    Initial commit. Create POST and GIT API

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.get("/", (req, res) => {
+  res.json("Hello commerce website!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(3000, (err) => {
+  if(err) console.log(err);
+  else console.log("Listening on port: ", 3000);
+});
